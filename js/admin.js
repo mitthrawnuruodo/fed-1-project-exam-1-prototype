30fe887aa24080a9169f0aacdf780ddfee356900
Lasse Hægland <seilenos@gmail.com>
2024-04-18 16:21:31 +0200

    Litt rydding

---
+++ js/admin.js
@@ -0,0 +1,17 @@
+console.log("Hello from admin.js")
+
+
+async function createPost() {
+
+    //let accessToken = <sett inn din token, feks. ved å hente fra LocalStorage>;
+ 
+    const options = {  
+      method: "POST",
+      headers: {    
+        Authorization: `Bearer ${accessToken}`,
+      }
+    } 
+    const response = await fetch(`https://v2.api.noroff.dev/blog/posts/xiaolasse`, options);
+    const data = await response.json();
+
+}
--- js/script.js
+++ js/script.js
@@ -12,9 +12,37 @@ async function getPosts() {
 function listPosts (posts) {
     //console.log(posts);
     const out = document.getElementById("posts");
+    let output = "";
     for (const post of posts) {
         console.log(post);
+        output += formatPost(post);
     }
+    out.innerHTML = output;
+}
+
+function formatPost(item) {
+    return `
+    <div>
+        <h2>${item.title}</h2>
+        <div>${item.body}</div>
+        <div>${item.id}</div>
+    </div>
+    `;
 }
 
 getPosts();
+
+async function createPost() {
+
+    //let accessToken = <sett inn din token, feks. ved å hente fra LocalStorage>;
+ 
+    const options = {  
+      method: "POST",
+      headers: {    
+        Authorization: `Bearer ${accessToken}`,
+      }
+    } 
+    const response = await fetch(`https://v2.api.noroff.dev/blog/posts/xiaolasse`, options);
+    const data = await response.json();
+
+}
